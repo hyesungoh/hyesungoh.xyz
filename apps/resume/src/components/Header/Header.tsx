d59327c20f6f0cac7fe8018a1c0bead882748a8d
Hyesung Oh <haesungoh414@gmail.com>
2023-05-15 17:30:27 +0900

    resume: 출력에 사용될 small 추가

--- apps/resume/src/components/Header/Header.tsx
+++ apps/resume/src/components/Header/Header.tsx
@@ -20,7 +20,13 @@ function Header({ heading, description }: IHeader) {
 
       <DescriptionWrapper>
         <Avatar src={authorImage.default.src} alt={authorName} text={authorName} size="xl" />
-        <P dangerouslySetInnerHTML={{ __html: description }}></P>
+        <Div>
+          <p>{description}</p>
+          {/* <small style={{ textDecoration: 'underline' }}>
+            해당 이력서는 <a href="https://resume.hyesungoh.xyz/">https://resume.hyesungoh.xyz</a>에서 보다 원활하게
+            확인하실 수 있습니다.
+          </small> */}
+        </Div>
       </DescriptionWrapper>
     </header>
   );
@@ -32,6 +38,7 @@ const HeadingWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: flex-start;
+  margin-bottom: 1.125rem;
 
   & > label:last-of-type {
     margin-top: 8px;
@@ -49,6 +56,6 @@ const DescriptionWrapper = styled.div`
   align-items: center;
 `;
 
-const P = styled.p`
+const Div = styled.div`
   margin-left: 0.875rem;
 `;
